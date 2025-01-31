065b1564f2186fa78b99b4ae297fabdc5ea46048
Davor Peuraca <davorpeu@gmail.com~>
2025-01-31 15:03:44 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-base:'/clue-one',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/clue-one/', // Add trailing slash
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
-
+  build: {
+    outDir: 'dist' // Explicitly set output directory (optional)
+  }
 })
