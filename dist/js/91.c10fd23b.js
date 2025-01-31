daada6a3091cb819e12019c4bebf9ceab136ef4a
Davor Peuraca <davorpeu@gmail.com~>
2025-01-31 14:31:00 +0100

    adding dist

---
+++ dist/js/91.c10fd23b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_project"]=self["webpackChunkvue_project"]||[]).push([[91],{91:(e,u,s)=>{s.r(u),s.d(u,{default:()=>l});var t=s(641);const a={class:"about"};function n(e,u){return(0,t.uX)(),(0,t.CE)("div",a,u[0]||(u[0]=[(0,t.Lk)("h1",null,"This is an about page",-1)]))}var r=s(262);const c={},o=(0,r.A)(c,[["render",n]]),l=o}}]);
+//# sourceMappingURL=91.c10fd23b.js.map
